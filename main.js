a23c862bfd75347f7a6b2ea734ffac233d9e8c06
Reb <rebeldomaker@icloud.com>
2026-09-20 19:05:21 +0200

    fixed JS syntax inside comments

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // //TIP With Search Everywhere, you can find any action, file, or symbol in your project. Press <shortcut actionId="Shift"/> <shortcut actionId="Shift"/>, type in <b>terminal</b>, and press <shortcut actionId="EditorEnter"/>. Then run <shortcut raw="npm run dev"/> in the terminal and click the link in its output to open the app in the browser.
-// export function setupCounter(element) {
+// export function setupCounter(element)
 //   //TIP Try <shortcut actionId="GotoDeclaration"/> on <shortcut raw="counter"/> to see its usages. You can also use this shortcut to jump to a declaration – try it on <shortcut raw="counter"/> on line 13.
 //   let counter = 0;
 
@@ -24,7 +24,6 @@
 
 //   //TIP Let’s see how to review and commit your changes. Press <shortcut actionId="GotoAction"/> and look for <b>commit</b>. Try checking the diff for a file – double-click main.js to do that.
 //   setCounter(0);
-// }
 
 // //TIP To find text strings in your project, you can use the <shortcut actionId="FindInPath"/> shortcut. Press it and type in <b>counter</b> – you’ll get all matches in one place.
 // setupCounter(document.getElementById('counter-value'));
